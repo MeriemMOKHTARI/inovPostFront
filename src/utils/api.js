3fb78d91ba2afea4bcac61263282e3d3453d662a
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-12-01 03:10:55 +0100

    sencond commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL =  'http://172.20.10.3:8080/feedback';
+const API_BASE_URL =  'http://127.0.0.1:8000/feedback';
 
 export const fetchFeedbacks = async (sourceFilter = 'all') => {
   try {
